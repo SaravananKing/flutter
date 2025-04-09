f6677fd67d651fb7bcaa75437347bf00169d52b4
Saravanan <115523510+SaravananKing@users.noreply.github.com>
2025-04-09 21:39:26 +0530

    Img.dart

--- lib/Img.dart
+++ lib/Img.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: ImageGallery()));
+void main() => runApp(MaterialApp(
+  home: ImageGallery(),
+  debugShowCheckedModeBanner: false,
+));
 
 class ImageGallery extends StatelessWidget {
   final List<String> images = [
-    'https://picsum.photos/200',
-    'https://picsum.photos/201',
-    'https://picsum.photos/202',
-    'https://picsum.photos/203',
-    'https://picsum.photos/204'
+    'https://picsum.photos/seed/1/600',
+    'https://picsum.photos/seed/2/600',
+    'https://picsum.photos/seed/3/600',
+    'https://picsum.photos/seed/4/600',
+    'https://picsum.photos/seed/5/600',
+    'https://picsum.photos/seed/6/600',
+    'https://picsum.photos/seed/7/600',
+    'https://picsum.photos/seed/8/600',
   ];
 
   @override
@@ -18,9 +24,55 @@ class ImageGallery extends StatelessWidget {
       body: GridView.builder(
         padding: EdgeInsets.all(10),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
         itemCount: images.length,
-        itemBuilder: (context, index) => Image.network(images[index], fit: BoxFit.cover),
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => FullScreenImage(imageUrl: images[index]),
+                ),
+              );
+            },
+            child: Hero(
+              tag: images[index],
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  images[index],
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FullScreenImage extends StatelessWidget {
+  final String imageUrl;
+
+  const FullScreenImage({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Hero(
+          tag: imageUrl,
+          child: InteractiveViewer(
+            child: Image.network(imageUrl),
+          ),
+        ),
       ),
     );
   }
